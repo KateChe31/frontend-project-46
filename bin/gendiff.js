c12e9302c8f8c8719def61a9fdc2cc2b7d959af2
Katerina Chernova <chkaterina1@gmail.com>
2025-03-26 13:56:42 +0400

    added changes to file gendiff for step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .parse(process.argv);
-  
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const format = program.opts().format;
+    console.log(`Comparing ${filepath1} and ${filepath2} with format: ${format}`);
+  });
+
+program.parse(process.argv);

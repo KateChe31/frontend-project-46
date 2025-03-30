27c0a06127a9b5217c20026140f5821561ff39c7
Katerina Chernova <chkaterina1@gmail.com>
2025-03-30 11:40:38 +0400

    step 4: changes to the file index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
+import _ from 'lodash';
+
 const genDiff = (data1, data2) => {
-    const keys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
-    const diffLines = [];
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   
-    keys.forEach(key => {
-      if (!(key in data1)) {
-        diffLines.push(`+ ${key}: ${data2[key]}`);
-      } else if (!(key in data2)) {
-        diffLines.push(`- ${key}: ${data1[key]}`);
-      } else if (data1[key] !== data2[key]) {
-        diffLines.push(`- ${key}: ${data1[key]}`);
-        diffLines.push(`+ ${key}: ${data2[key]}`);
-      } else {
-        diffLines.push(`  ${key}: ${data1[key]}`);
-      }
-    });
-  
-    return diffLines.join('\n');
-  };
-  
-  export default genDiff;
+  const diffLines = keys.flatMap((key) => {
+    const hasInFirst = _.has(data1, key);
+    const hasInSecond = _.has(data2, key);
+
+    if (!hasInSecond) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!hasInFirst) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return [
+        `  - ${key}: ${data1[key]}`,
+        `  + ${key}: ${data2[key]}`
+      ];
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${diffLines.join('\n')}\n}`;
+};
+
+export default genDiff;
   
